f721ded64b0749e61cec34d1cb3fc2b3b407e71a
Matthew Norman <mn.nevada@protonmail.com>
2025-11-07 19:02:19 -0800

    began initial mongo database setup with mongoose schema

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/app/ui/products/products.module.css";
--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Inter } from 'next/font/google'
 import './ui/globals.css'
 
