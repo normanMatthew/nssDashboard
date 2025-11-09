9d86a8d214d80489d6eea13af222489d92b36a35
Matthew Norman <mn.nevada@protonmail.com>
2025-11-09 09:14:19 -0800

    remove .env file from tracking and add to .gitignore

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,14 +1,26 @@
-//set up database schema.
 import mongoose from 'mongoose';
 
 // Load environment variables from .env file.
 import dotenv from 'dotenv';
 dotenv.config({ path: '/app/.env' });
 
+//persistent connection object (kept in module scope). Connection is declared outside function so it persists between calls.
+const connection = {};
+
 // Connect to MongoDB database.
 export const connectToDatabase = async () => {
+    //Check if already connected. If so, reuse existing connection.
+    if (connection.isConnected) {
+        console.log('Using existing MongoDB database connection');
+        return;
+    }
+
     try {
-        await mongoose.connect(process.env.CONNECTION_STRING);
+        const db = await mongoose.connect(process.env.CONNECTION_STRING);
+
+        //Retrieve connection status.
+        connection.isConnected = db.connections[0].readyState;
+
         console.log('Connected to MongoDB database successfully');
     } catch (error) {
         console.error('Error connecting to MongoDB database:', error);
