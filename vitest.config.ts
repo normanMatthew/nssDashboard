aabbe9707147b36f1b872767dd4c8b4d825ebf15
Matthew Norman <mn.nevada@protonmail.com>
2025-11-13 15:53:58 -0800

    updated yaml to included smart ci/cd pipeline, made adjustments to vitest.config.ts to include coverage, added test:changed script to package.json

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,5 +9,17 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom', // ✅ safer, faster, ESM-compatible
     setupFiles: './vitest.setup.ts',
+
+    //coverage - vitest moved coverage thresholds into a nested 'threshold' object in newer versions
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      thresholds: {
+        lines: 85,
+        functions: 85,
+        branches: 80,
+        statements: 85,
+      }
+    },
   },
 })
